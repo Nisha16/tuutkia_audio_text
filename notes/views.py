961bf824dcd9453040c58549dd474893ebcc06d0
Vidya <vidyags@outlook.com>
2017-04-10 14:20:19 -0700

    Modified template and fixed bugs for dependency

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Note
 
 def note_list(request):
-    return render(request, 'notes/note_list.html',{})
+    notes = Note.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'notes/note_list.html',{'notes':notes})
